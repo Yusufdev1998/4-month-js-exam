f930c6d7da933a3cf18dd3697ab087c1f5ada3dc
yusufdev1998 <yusufdev1998@gmail.com>
2023-12-07 13:34:34 +0500

    finished

---
+++ 2-variant.js
@@ -0,0 +1,37 @@
+// Masalalarni kodlarini yurgizib ko'rib tekshirish mumkin emas!!!
+
+// 1-task
+// funksiya string va index qabul qiladi. Ushbu funksiya stringni ichidagi
+// ushbu indexdagi belgini qaytarsin.
+// Namuna:
+// myString("salom", 1) => 'a';
+// myString("xayr", 10) => 'topilmadi!'
+
+// 2-task
+// string ichida raqam borligini aniqlaydigan funksiya yasang.
+// Namunalar:
+// myNumber("salom5") => true;
+// myNumber("javaScript") => false;
+
+// 3-task
+// stringni ichidan oxirigi 3ta belgisini ajratib beradigan
+// funksiya yasang.
+// Namuna:
+// myString("olamda yagona") => 'ona';
+// myString("yoshi ulug' Tayota")=> 'ota'
+// myString("bu")=> '3ta belgi olishni imkoni bo'lmadi';
+
+// 4-task
+// stringning boshidan 3ta belgisini olib tashlaydigan funksiya yasang.
+// Namuna:
+// myString('jahon') => "on";
+
+// 5-task
+// funksiya 6ta parameter qabul qiladi. myFunc(a,b,c,d,e,f);
+// 1. ani bga qo'shing
+// 2. keyin cni ayring.
+// 3. keyin dga ko'paytiring
+// 4. keyin ega bo'ling
+// 5. keyin f darajaga ko'taring.
+// Namun:
+// myFunc(6,5,4,3,2,1) => 10.5
